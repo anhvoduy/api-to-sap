64767c5843a6ec8dc5b1e6a782cb32f47a6164b8
Anh <voduyanh1984@gmail.com>
2020-08-06 17:46:04 +0700

    - make connect to SAP

---
+++ NetCoreToSap/Controllers/StatusController.cs
@@ -0,0 +1,26 @@
+﻿namespace NetCoreToSap.Controllers
+{
+    using System;    
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : BaseController
+    {
+        private readonly IConfiguration _config;
+
+        public StatusController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Status()
+        {
+            return Ok(HandleResponse());
+        }
+    }
+}
---
+++ NetCoreToSap/Utilities/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreToSap.Utilities
+{
+    public class Utility
+    {
+        public const string inventoryKey = "inventoryKey";
+
+        public const string MyConnectionString = "MyConnectionString";
+
+        public const string DateFormatString = "yyyy-MM-dd";
+    }
+
+    public static class MessageConstant
+    {
+        public const string COMPLETED = "{0} completed.";
+        public const string SUCCSESS = "success";
+        public const string CONNECTED = "{0} connected";
+        public const string NOTCONNECTED = "{0} not connected";
+    }
+}
